f15d37a931e7c8f050ca7745918daf053511c0c6
Matheus <matheus_501@hotmail.com>
2024-07-26 20:56:24 -0300

    refatorando codigo

--- src/store/features/listProducts/Slice.ts
+++ src/store/features/listProducts/Slice.ts
@@ -1,62 +1,27 @@
-// import { createSlice } from '@reduxjs/toolkit';
-// import type { PayloadAction } from '@reduxjs/toolkit';
-
-// export interface IProduct {
-//   id: string;
-//   name: string;
-// }
-// export interface IListProducts {
-//   products: Array<IProduct>;
-// }
-
-// const initialState: IListProducts = {
-//   products: [],
-// };
-
-// export const listProductsSlice = createSlice({
-//   name: 'listProducts',
-//   initialState,
-//   reducers: {
-//     setNewProduct: (state, action: PayloadAction<IProduct>) => {
-//       state.products.push(action.payload);
-//       localStorage.setItem('products', JSON.stringify(state.products));
-//     },
-//     deleteProduct: (state, action: PayloadAction<string>) => {
-//       state.products = state.products.filter(
-//         (product) => product.id !== action.payload
-//       );
-//     },
-//   },
-// });
-
-// export const { setNewProduct, deleteProduct } = listProductsSlice.actions;
-
-// export default listProductsSlice.reducer;
-
-// store/features/listProducts/Slice.ts
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 export interface IProduct {
   id: string;
   name: string;
 }
-
-interface ListProductsState {
-  products: IProduct[];
+export interface IListProducts {
+  products: Array<IProduct>;
 }
 
-const initialState: ListProductsState = {
+const initialState: IListProducts = {
   products: [],
 };
 
-const listProductsSlice = createSlice({
+export const listProductsSlice = createSlice({
   name: 'listProducts',
   initialState,
   reducers: {
-    setNewProduct(state, action: PayloadAction<IProduct>) {
+    setNewProduct: (state, action: PayloadAction<IProduct>) => {
       state.products.push(action.payload);
+      localStorage.setItem('products', JSON.stringify(state.products));
     },
-    deleteProduct(state, action: PayloadAction<string>) {
+    deleteProduct: (state, action: PayloadAction<string>) => {
       state.products = state.products.filter(
         (product) => product.id !== action.payload
       );
